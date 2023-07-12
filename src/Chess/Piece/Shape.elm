d76fe5f64ccd708566d966aaab90cbf7841a8e10
ioanniskatsaros <katsaros.ioannis@protonmail.com>
2023-07-13 01:23:54 +0200

    WIP add board image

--- src/Chess/Piece/Shape.elm
+++ src/Chess/Piece/Shape.elm
@@ -1,6 +1,6 @@
 module Chess.Piece.Shape exposing (..)
 
-import Playground exposing (Color, Shape, circle, group, moveDown, moveUp, polygon, rectangle, rotate, triangle)
+import Playground exposing (..)
 
 
 pawn : Color -> Shape
@@ -15,15 +15,16 @@ bishop : Color -> Shape
 bishop c =
     group
         [ triangle c 40 |> moveDown 35
-        , triangle c 40 |> moveUp 10
+        , triangle c 40
         ]
 
 
 knight : Color -> Shape
 knight c =
     group
-        [ polygon c [ ( -30, 0 ), ( 30, 60 ), ( 30, 0 ) ] |> moveDown 10
-        , triangle c 40 |> moveDown 35
+        [ triangle c 40 |> moveDown 35
+        , oval c 30 60 |> move -10 0 |> rotate -45
+        , triangle c 15 |> move 0 18
         ]
 
 
@@ -47,3 +48,53 @@ queen c =
         , triangle c 40 |> rotate 180
         , triangle c 40
         ]
+
+
+board : Color -> Color -> Shape
+board black_ white_ =
+    let
+        flip : (a -> b -> c) -> b -> a -> c
+        flip f b a =
+            f a b
+
+        placeSquares : List (Color -> Shape) -> List Shape
+        placeSquares =
+            List.indexedMap
+                (\n sq ->
+                    let
+                        c =
+                            modBy 8 n
+
+                        l =
+                            n // 8
+                    in
+                    move
+                        (toFloat (80 * c))
+                        (toFloat (80 * l))
+                        (sq
+                            (if
+                                (modBy 2 l == 1 && modBy 2 c == 0)
+                                    || (modBy 2 l == 0 && modBy 2 c == 1)
+                             then
+                                white_
+
+                             else
+                                black_
+                            )
+                        )
+                )
+
+        {-
+
+           B | W | B
+           _ _ _ _ _
+           W | B | W
+           _ _ _ _ _
+           B | W | B
+        -}
+    in
+    List.repeat 64 (flip square 80)
+        |> placeSquares
+        |> group
+        |> move -256 -256
+        |> scale 0.8
--- src/Main.elm
+++ src/Main.elm
@@ -25,12 +25,7 @@ initModel =
 view : P.Computer -> Model -> List P.Shape
 view _ model =
     List.map (P.moveLeft 80)
-        [ Piece.pawn P.gray |> P.moveLeft 80
-        , Piece.bishop P.black |> P.moveDown 80
-        , Piece.knight P.red
-        , Piece.rook P.blue |> P.moveUp 80
-        , Piece.king P.green |> P.moveRight 80
-        , Piece.queen P.orange |> P.move 80 80
+        [ Piece.board P.black P.white
         ]
 
 
